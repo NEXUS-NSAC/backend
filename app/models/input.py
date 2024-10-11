166836470003fe46315b5f01e5f041a84ef05bee
DhanukaSheyamPerera <dhanuka.sp@outlook.com>
2024-10-11 23:41:17 +0530

    fully working backend

--- app/models/input.py
+++ app/models/input.py
@@ -5,30 +5,38 @@ from typing import Optional
 
 
 class InputBaseModel(BaseModel):
-    extreme_heat: Optional[int] = Field(0, alias="extremeHeat")
-    tropical_cyclones: Optional[int] = Field(0, alias="tropicalCyclones")
-    earthquakes_and_volcanoes: Optional[int] = Field(0, alias="earthquakesAndVolcanoes")
-    floods: Optional[int] = Field(0, alias="floods")
-    landslides: Optional[int] = Field(0, alias="landslides")
-    global_primary_energy_consumption: Optional[int] = Field(
+    extreme_heat: Optional[int | float | str] = Field(0, alias="extremeHeat")
+    tropical_cyclones: Optional[int | float | str] = Field(0, alias="tropicalCyclones")
+    earthquakes_and_volcanoes: Optional[int | float | str] = Field(
+        0, alias="earthquakesAndVolcanoes"
+    )
+    floods: Optional[int | float | str] = Field(0, alias="floods")
+    landslides: Optional[int | float | str] = Field(0, alias="landslides")
+    global_primary_energy_consumption: Optional[int | float | str] = Field(
         0, alias="globalPrimaryEnergyConsumption"
     )
-    air_quality: Optional[int] = Field(0, alias="airQuality")
-    glacier_mass_balance: Optional[int] = Field(0, alias="glacierMassBalance")
-    precipitation: Optional[int] = Field(0, alias="precipitation")
-    greenhouse_gases: Optional[int] = Field(0, alias="greenhouseGases")
-    atmospheric_temperature: Optional[float] = Field(
+    air_quality: Optional[int | float | str] = Field(0, alias="airQuality")
+    glacier_mass_balance: Optional[int | float | str] = Field(
+        0, alias="glacierMassBalance"
+    )
+    precipitation: Optional[int | float | str] = Field(0, alias="precipitation")
+    greenhouse_gases: Optional[int | float | str] = Field(0, alias="greenhouseGases")
+    atmospheric_temperature: Optional[float | int] = Field(
         0.0, alias="atmosphericTemperature"
     )
-    solar_radiation_absorption: Optional[int] = Field(
+    solar_radiation_absorption: Optional[int | float | str] = Field(
         0, alias="solarRadiationAbsorption"
     )
-    erosion_rates: Optional[int] = Field(0, alias="erosionRates")
-    snow_cover_duration: Optional[int] = Field(0, alias="snowCoverDuration")
+    erosion_rates: Optional[float] = Field(0, alias="erosionRates")
+    snow_cover_duration: Optional[int | float | str] = Field(
+        0, alias="snowCoverDuration"
+    )
     sea_surface_temperature: Optional[float] = Field(0.0, alias="seaSurfaceTemperature")
-    water_quality_indicators: Optional[int] = Field(0, alias="waterQualityIndicators")
-    forest_cover: Optional[int] = Field(0, alias="forestCover")
-    soil_composition_and_quality: Optional[int] = Field(
+    water_quality_indicators: Optional[int | float | str] = Field(
+        0, alias="waterQualityIndicators"
+    )
+    forest_cover: Optional[int | float | str] = Field(0, alias="forestCover")
+    soil_composition_and_quality: Optional[int | float | str] = Field(
         0, alias="soilCompositionAndQuality"
     )
 
@@ -37,17 +45,22 @@ class InputBaseModel(BaseModel):
 
 
 class OutputBaseModel(InputBaseModel):
-    animal_impact: Optional[int] = Field(0, alias="animalImpact")
-    human_impact: Optional[int] = Field(0, alias="humanImpact")
-    tree_impact: Optional[int] = Field(0, alias="treeImpact")
-    aquatic_impact: Optional[int] = Field(0, alias="aquaticImpact")
+    animal_impact: Optional[int | float | str] = Field(0, alias="animalImpact")
+    human_impact: Optional[int | float | str] = Field(0, alias="humanImpact")
+    tree_impact: Optional[int | float | str] = Field(0, alias="treeImpact")
+    aquatic_impact: Optional[int | float | str] = Field(0, alias="aquaticImpact")
 
     class Config:
         populate_by_alias = True
 
 
+class InputModel(BaseModel):
+    parameters: Optional[dict] = Field(default={})
+    date: Optional[str] = Field(None, alias="date")
+
+
 class ModifiedOutputModelBase(OutputBaseModel):
-    diasasters: Optional[int] = Field(0, alias="disasters")
+    diasasters: Optional[int | float | str] = Field(0, alias="disasters")
     date: Optional[str] = Field(None, alias="date")
 
     class Config:
--- app/routes/generate.py
+++ app/routes/generate.py
@@ -1,7 +1,20 @@
+import json
 from fastapi import APIRouter
-from models.input import InputBaseModel, OutputBaseModel, ModifiedOutputModelBase
+from models.input import (
+    InputBaseModel,
+    OutputBaseModel,
+    ModifiedOutputModelBase,
+    InputModel,
+)
+from openai import OpenAI
+from typing import Final
+from dotenv import load_dotenv
+
+load_dotenv()
 
 router = APIRouter()
+client: Final = OpenAI()
+assistant_id: Final = "asst_O20YXfM20bjFTS8uLn2gxokD"
 
 
 @router.get("/")
@@ -9,33 +22,64 @@ async def read_root():
     return {"message": "Flying to the stars..."}
 
 
-@router.post("/{date}", response_model=ModifiedOutputModelBase)
+@router.post("/{date}", response_model=dict)
 async def generate(date: str, input: InputBaseModel):
+
+    content = InputModel(parameters=input.model_dump(), date=date)
+
+    thread = client.beta.threads.create()
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=content.model_dump_json(),
+    )
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+        # instructions="Please address the user as Jane Doe. The user has a premium account.",
+    )
+    if run.status == "completed":
+        messages = client.beta.threads.messages.list(thread_id=thread.id)
+        # print(messages)
+    else:
+        print(run.status)
+
+    message_content = messages.data[0].content[0].text.value
+    message_content = message_content.strip("```json").strip("```")
+    message_content = json.loads(message_content)
+    print(message_content)
+
     def normalize(value, min_value, max_value):
         return 1 + (value - min_value) * (5 - 1) / (max_value - min_value)
 
     min_value = min(
-        input.extreme_heat,
-        input.tropical_cyclones,
-        input.earthquakes_and_volcanoes,
-        input.floods,
-        input.landslides,
+        int(message_content["extreme_heat"]),
+        int(message_content["tropical_cyclones"]),
+        int(message_content["earthquakes_and_volcanoes"]),
+        int(message_content["floods"]),
+        int(message_content["landslides"]),
     )
     max_value = max(
-        input.extreme_heat,
-        input.tropical_cyclones,
-        input.earthquakes_and_volcanoes,
-        input.floods,
-        input.landslides,
+        int(message_content["extreme_heat"]),
+        int(message_content["tropical_cyclones"]),
+        int(message_content["earthquakes_and_volcanoes"]),
+        int(message_content["floods"]),
+        int(message_content["landslides"]),
     )
 
-    norm_extreme_heat = normalize(input.extreme_heat, min_value, max_value)
-    norm_tropical_cyclones = normalize(input.tropical_cyclones, min_value, max_value)
-    norm_earthquakes_and_volcanoes = normalize(
-        input.earthquakes_and_volcanoes, min_value, max_value
+    norm_extreme_heat = normalize(
+        int(message_content["extreme_heat"]), min_value, max_value
     )
-    norm_floods = normalize(input.floods, min_value, max_value)
-    norm_landslides = normalize(input.landslides, min_value, max_value)
+    norm_tropical_cyclones = normalize(
+        int(message_content["tropical_cyclones"]), min_value, max_value
+    )
+    norm_earthquakes_and_volcanoes = normalize(
+        int(message_content["earthquakes_and_volcanoes"]), min_value, max_value
+    )
+    norm_floods = normalize(int(message_content["floods"]), min_value, max_value)
+    norm_landslides = normalize(
+        int(message_content["landslides"]), min_value, max_value
+    )
 
     total_sum = (
         norm_extreme_heat
@@ -47,17 +91,8 @@ async def generate(date: str, input: InputBaseModel):
 
     average = total_sum / 5
 
-    return ModifiedOutputModelBase(
-        **input.model_dump(
-            exclude={
-                "extremeHeat",
-                "tropicalCyclones",
-                "earthquakesAndVolcanoes",
-                "floods",
-                "landslides",
-            },
-            by_alias=True,
-        ),
-        disasters=average,
-        date=date
-    )
+    return {
+        "content": message_content,
+        "disasters": average,
+        "date": date,
+    }
